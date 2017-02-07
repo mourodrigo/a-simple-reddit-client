0f588fd4ba48a77f67bd92711603f9865915776d
mourodrigo <rodrigobt20@gmail.com>
2017-02-07 21:48:09 -0200

    Xcode project Init with basic configuration. Closes #2

---
+++ a-simple-reddit-client/a-simple-reddit-client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  a-simple-reddit-client
+//
+//  Created by Rodrigo Bueno Tomiosso on 07/02/17.
+//  Copyright © 2017 mourodrigo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
