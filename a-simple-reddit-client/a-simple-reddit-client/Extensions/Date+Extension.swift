2f7d5be5595963e343f5a137cf2453e3cb3faa57
Rodrigo Bueno Tomiosso <mourodrigo_tres@new-host-7.home>
2017-02-08 14:36:52 -0200

    Created date extension method for presenting "x hours ago". Issue #3

--- a-simple-reddit-client/a-simple-reddit-client/Extensions/Date+Extension.swift
+++ a-simple-reddit-client/a-simple-reddit-client/Extensions/Date+Extension.swift
@@ -17,4 +17,42 @@ extension Date {
         }
     }
     
+    func timeAgoString() -> String {
+        let calendar = Calendar.current
+        let now = Date()
+        let earliest = (now as NSDate).earlierDate(self)
+        let latest = (earliest == now) ? self : now
+        let components:DateComponents = (calendar as NSCalendar).components([NSCalendar.Unit.minute , NSCalendar.Unit.hour , NSCalendar.Unit.day , NSCalendar.Unit.weekOfYear , NSCalendar.Unit.month , NSCalendar.Unit.year , NSCalendar.Unit.second], from: earliest, to: latest, options: NSCalendar.Options())
+        
+        if (components.year! >= 2) {
+            return "\(components.year!) years ago"
+        } else if (components.year! >= 1){
+            return "1 year ago"
+        } else if (components.month! >= 2) {
+            return "\(components.month!) months ago"
+        } else if (components.month! >= 1){
+            return "1 month ago"
+        } else if (components.weekOfYear! >= 2) {
+            return "\(components.weekOfYear!) weeks ago"
+        } else if (components.weekOfYear! >= 1){
+            return "1 week ago"
+        } else if (components.day! >= 2) {
+            return "\(components.day!) days ago"
+        } else if (components.day! >= 1){
+            return "1 day ago"
+        } else if (components.hour! >= 2) {
+            return "\(components.hour!) hours ago"
+        } else if (components.hour! >= 1){
+            return "1 hour ago"
+        } else if (components.minute! >= 2) {
+            return "\(components.minute!) minutes ago"
+        } else if (components.minute! >= 1){
+            return "1 minute ago"    
+        } else if (components.second! >= 3) {
+            return "\(components.second!) seconds ago"
+        } else {
+            return "Just now"
+        }
+    }
+
 }
