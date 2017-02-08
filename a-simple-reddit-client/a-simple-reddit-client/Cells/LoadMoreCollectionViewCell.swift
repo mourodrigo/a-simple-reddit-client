d1204588403f902718256dc2f91fcb49a2d77607
Rodrigo Bueno Tomiosso <mourodrigo_tres@new-host-7.home>
2017-02-08 19:17:15 -0200

    Created LoadMoreCollectionViewCell for pagination. Issue #8

---
+++ a-simple-reddit-client/a-simple-reddit-client/Cells/LoadMoreCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  LoadMoreCollectionViewCell.swift
+//  a-simple-reddit-client
+//
+//  Created by Rodrigo Bueno Tomiosso on 08/02/17.
+//  Copyright © 2017 mourodrigo. All rights reserved.
+//
+
+import UIKit
+
+class LoadMoreCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var loadMoreLabel: UILabel!
+    @IBOutlet var activityView: UIActivityIndicatorView!
+    
+}
