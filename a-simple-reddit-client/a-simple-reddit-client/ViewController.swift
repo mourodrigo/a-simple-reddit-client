e906a19c1d11a4f320e0d8510053e5d0af35f03d
mourodrigo <rodrigobt20@gmail.com>
2017-02-07 23:42:44 -0200

    Created login button for login on ViewController Login button redirecting user to safari on reddit authorization page

--- a-simple-reddit-client/a-simple-reddit-client/ViewController.swift
+++ a-simple-reddit-client/a-simple-reddit-client/ViewController.swift
@@ -19,7 +19,35 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func didPressLoginButton(_ sender: AnyObject) {
+        let clientId = "isBHEDq__wuqTQ"
+        let responseType = "code"
+        let state = "UNIQUERANDOMSTRING"
+        let redirect_uri = "mourodrigo.a-simple-reddit-client://callback" //can get it directly from info plist if necessary
+        let duration = "permanent"
+        let scope = "read"
+        
+        let authUrl = "https://www.reddit.com/api/v1/authorize.compact?client_id=\(clientId)&response_type=\(responseType)&state=\(state)&redirect_uri=\(redirect_uri)&duration=\(duration)&scope=\(scope)"
+        
+        openOnBrowser(url: authUrl)
+    }
+    
+    func openOnBrowser(url: String) {
+        if let url = URL(string: url) {
+            if #available(iOS 10, *) {
+                UIApplication.shared.open(url, options: [:],
+                                          completionHandler: {
+                                            (success) in
+                                            print("Open \(url): \(success)")
+                })
+            } else {
+                let success = UIApplication.shared.openURL(url)
+                print("Open \(url): \(success)")
+            }
+        }
+    }
+    
+    
 }
 
